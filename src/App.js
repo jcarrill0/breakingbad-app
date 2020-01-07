1d10083de7ed882a3e725102c44298bb149793e2
unknown <jcarrillo@SJ-STA-001-L349.clarocr.americamovil.ca1>
2020-01-06 22:55:40 -0600

    Primer commit app breakingbad

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+function Frase({frase}) {
+  return (
+    <div className="frase">
+        <h1>{frase.quote}</h1>
+        <p>- {frase.author}</p>
+    </div>
+  )
+}
 
 function App() {
+
+  const [frase, obtenerFrase] = useState({});
+
+  const consultarAPI = async () => {
+    // const consulta = await fetch('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    // const resultado = await consulta.json();
+    const resultado = await axios('http://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    // agregar el resultado de la api al state 
+    obtenerFrase(resultado.data[0])
+  }
+
+  // consulta a una rest API
+  useEffect(
+    () => {
+      consultarAPI()
+    },[]); 
+
+    console.log(frase);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+        <Frase 
+          frase={frase}
+        />
+        <button
+          onClick={consultarAPI}
+        >Nueva Frase</button>
     </div>
   );
 }
